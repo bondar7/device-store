9fe0323b22b40d7c9c08630735cd580a770c0bc9
Maksym <maksimbondar225@gmail.com>
2025-02-17 23:15:59 +0100

    export in wishlistRouter.js

--- server/routes/wishlistRouter.js
+++ server/routes/wishlistRouter.js
@@ -6,3 +6,5 @@ const wishlistController = require('../controllers/wishlistController');
 router.get('/', authMiddleware, wishlistController.getUserWishlist);
 router.post('/add', authMiddleware, wishlistController.add);
 router.delete('/delete/:deviceId', authMiddleware, wishlistController.delete);
+
+module.exports = router;
